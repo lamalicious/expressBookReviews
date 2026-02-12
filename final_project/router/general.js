c156c2a4d96e14d5aee20172d35bc7f47fa6c046
lamalicious <veronicamotta96@gmail.com>
2026-02-12 13:20:09 -0500

    Complete the code for getting the book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,12 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        res.send(books[isbn]);
+    } else {
+        res.status(404).send({ message: "Book not found" });
+    }
 });
 
 // Get book details based on author
