ab8f1342ec76e01d9bfb261cedd1f64da88b72f3
lamalicious <veronicamotta96@gmail.com>
2026-02-13 08:49:09 -0500

    Complete the code for getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,8 +53,13 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    const bookRequired = books[isbn]
+    if (bookRequired) {
+        return res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
+    } else {
+        return res.status(404).json({ message: "No books with that ISBN." });
+    }
 });
 
 module.exports.general = public_users;
